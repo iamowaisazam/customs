f38d12953fcbd9c0e37f8da45fb901bcfc67badc
iamowaisazam <iamowaisazam@gmail.com>
2024-10-25 00:33:45 +0500

    update

--- resources/views/admin/delivery-challans/print.blade.php
+++ resources/views/admin/delivery-challans/print.blade.php
@@ -6,10 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DELIVERY CHALLAN</title>
 
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
-
-    <link rel="stylesheet" href="http://localhost/customs/public/admin/assets/css/style.css">
-
+    <link href="{{asset('admin/assets/css/style.css')}}" rel="stylesheet">
     <style>
         .content{
             max-width: 800px;
